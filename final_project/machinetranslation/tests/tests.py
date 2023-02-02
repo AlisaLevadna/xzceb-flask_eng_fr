d21234a5d93ceccf66d36ce84272ee4ac71c6ba8
AlisaLevadna <alisa.levadna@nure.ua>
2023-02-02 17:38:34 -0500

    Final changes

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,16 @@
+import unittest
+from translator import french_to_english, english_to_french
+
+class TestTranslator(unittest.TestCase):
+
+    def test_french_to_english(self):
+        self.assertNotEqual(french_to_english('Bonjour'), 'Bonjour')
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
+
+    def test_english_to_french(self):
+        self.assertNotEqual(english_to_french('Hello'), 'Hello')
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+
+
+if __name__ == '__main__':
+    unittest.main()
